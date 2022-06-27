daebfc3990e3be176bfa6a131a8d42dd544ba8c8
Erwin Rivera <erwin.j.rivera@outlook.com>
2022-06-27 18:32:02 +0800

    2nd commit

--- MouseMover/MouseMover/Form1.cs
+++ MouseMover/MouseMover/Form1.cs
@@ -1,3 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace MouseMover
 {
     public partial class Form1 : Form
@@ -5,18 +15,6 @@ namespace MouseMover
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            this.Cursor = new Cursor(Cursor.Current.Handle);
-            Cursor.Position = new Point(Cursor.Position.X - 50, Cursor.Position.Y - 50);
-            Cursor.Clip = new Rectangle(this.Location, this.Size);
         }
     }
 }
